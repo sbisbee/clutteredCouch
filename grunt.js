5cf2cc814ca7afe11d65acc81b5721e6b11435c3
Sam Bisbee <sam@sbisbee.com>
2012-07-03 16:34:15 -0400

    grunt files

---
+++ grunt.js
@@ -0,0 +1,49 @@
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    pkg: '<json:package.json>',
+    test: {
+      files: ['test/**/*.js']
+    },
+    lint: {
+      files: ['grunt.js', 'lib/**/*.js', 'test/**/*.js']
+    },
+    watch: {
+      files: '<config:lint.files>',
+      tasks: 'default'
+    },
+    jshint: {
+      options: {
+        // enforcing options
+        bitwise: false,
+        curly: true,
+        eqeqeq: true,
+        forin: true,
+        immed: true,
+        latedef: true,
+        newcap: true,
+        noarg: true,
+        noempty: true,
+        nonew: true,
+        undef: true,
+        trailing: true,
+
+        // relaxing options
+        boss: true,
+        eqnull: true,
+        devel: true,
+
+        // environments
+        node: true
+      },
+      globals: {
+        exports: true
+      }
+    }
+  });
+
+  // Default task.
+  grunt.registerTask('default', 'lint test');
+
+};
